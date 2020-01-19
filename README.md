be12a6ed3cf28bbebd5731f5fc4661666350b922
Gwannon <monclus.jorge@gmail.com>
2020-01-19 10:21:21 +0100

    Animación en hover

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # pixelCSS
+
+pixelCSS es un miniframework que te permite crear PixelArt con CSS y Html.
+
+Permite crear item de 24x24 y animarlos con hover.
--- tools/cssGenerator.php
+++ tools/cssGenerator.php
@@ -4,7 +4,9 @@ $size = 24;
 
 for ($i = 1; $i <= $size; $i ++) {
   echo ".sprite > p.w".$i.", .sprite.w".$i." { width: calc(var(--pixel-size) * ".$i."); }\n";
-  echo ".sprite > p.h".$i.", .sprite.h".$i." { height: calc(var(--pixel-size) * ".$i."); }\n\n";
+  echo ".sprite > p.h".$i.", .sprite.h".$i." { height: calc(var(--pixel-size) * ".$i."); }\n";
+  echo ".sprite:hover > p.hover-w".$i." { width: calc(var(--pixel-size) * ".$i."); }\n";
+  echo ".sprite:hover > p.hover-h".$i." { height: calc(var(--pixel-size) * ".$i."); }\n\n";
 }
 
 echo ".r1 { top: 0%; }\n";
@@ -14,7 +16,9 @@ for ($i = 2; $i <= $size; $i ++) {
   for ($y = 2; $y <= $i; $y ++) {
     $step = (100 / $i);
     echo ".h".$i." p.r".$y." { top: ".round(($step * ($y-1)), 2, PHP_ROUND_HALF_DOWN)."%; }\n";
-    echo ".w".$i." p.c".$y." { left: ".round(($step * ($y-1)), 2, PHP_ROUND_HALF_DOWN)."%; }\n\n";
+    echo ".w".$i." p.c".$y." { left: ".round(($step * ($y-1)), 2, PHP_ROUND_HALF_DOWN)."%; }\n";
+    echo ".h".$i.":hover p.hover-r".$y." { top: ".round(($step * ($y-1)), 2, PHP_ROUND_HALF_DOWN)."%; }\n";
+    echo ".w".$i.":hover p.hover-c".$y." { left: ".round(($step * ($y-1)), 2, PHP_ROUND_HALF_DOWN)."%; }\n\n";
   }
 }
 
