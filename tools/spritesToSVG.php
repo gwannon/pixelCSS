7c6e9259adfcd60a46af7585b26a7f016e8fc87a
Gwannon <monclus.jorge@gmail.com>
2020-12-05 11:14:25 +0100

    Update animations frames12

--- tools/spritesToSVG.php
+++ tools/spritesToSVG.php
@@ -1,10 +1,17 @@
 <?php
-  $imageUrls[] = "temp/guybrush1.png";
-  $imageUrls[] = "temp/guybrush2.png";
-  $imageUrls[] = "temp/guybrush3.png";
-  $imageUrls[] = "temp/guybrush4.png";
-  $imageUrls[] = "temp/guybrush5.png";
-  $imageUrls[] = "temp/guybrush6.png";
+  $imageUrls[0] = "temp/stan0.png";
+  $imageUrls[1] = "temp/stan1.png";
+  $imageUrls[2] = "temp/stan2.png";
+  $imageUrls[3] = "temp/stan3.png";
+  $imageUrls[4] = "temp/stan4.png";
+  $imageUrls[5] = "temp/stan5.png";
+  $imageUrls[6] = "temp/stan6.png";
+  $imageUrls[7] = "temp/stan7.png";
+  $imageUrls[8] = "temp/stan8.png";
+  $imageUrls[9] = "temp/stan9.png";
+  $imageUrls[10] = "temp/stan10.png";
+  $imageUrls[11] = "temp/stan11.png";
+  $imageUrls[12] = "temp/stan12.png";
   $pixel_size = 5;
   list($width, $height, $type, $attr) = getimagesize($imageUrls[0]);
 ?>
@@ -16,13 +23,13 @@
     <link href="../src/css/cssPixel.css" rel="stylesheet">
   </head>
   <body class="black">
-    <svg width="<?php echo ($width * $pixel_size); ?>" height="<?php echo ($height * $pixel_size); ?>" class="animation frames<?php echo count($imageUrls); ?>">
+    <svg width="<?php echo ($width * $pixel_size); ?>" height="<?php echo ($height * $pixel_size); ?>" class="animation frames<?php echo (isset($imageUrls[0]) ? count($imageUrls)-1 : count($imageUrls)); ?>">
       <?php
         foreach ($imageUrls as $key => $imageUrl) {
           list($width, $height, $type, $attr) = getimagesize($imageUrl);
           $im = imagecreatefrompng($imageUrl);
           ?>
-        <g class="sprite"  animation="<?php echo ($key+1); ?>">
+        <g class="sprite"  animation="<?php echo $key; ?>">
           <?php
             $size = 1;
             for($r = 1; $r <= $height; $r++) {
